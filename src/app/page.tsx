75c552062fd4f0f53614c53ca2fd6d026e0c7a04
Cezar D Silva <cezardsilva@gmail.com>
2025-05-26 20:12:24 -0300

    inicio do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,88 @@
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from "lucide-react"
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"/>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"/>
+            <div className="w-3 h-3 bg-green-500 rounded-full"/>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <nav className="space-y-5 mt-10">
+            <a href="#" className="flex items-center gap-3 text-xs font-semibold text-zinc-300" >
+              <HomeIcon/>
+              Home
+            </a>
+            <a href="#" className="flex items-center gap-3 text-xs font-semibold text-zinc-300">
+              <Search/>
+              Search
+            </a>
+            <a href="#" className="flex items-center gap-3 text-xs font-semibold text-zinc-200">
+              <Library/>
+              Your Libary
+            </a>
+          </nav>
+
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Hot Hits Brasil</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Aniver Funk</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Ny Playlist</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100" >Playlist 1</a>
+
+          </nav>
+
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="text-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4">
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+            <div className="bg-white/10 rouded">
+            <Image src="/album.jpg" alt="Capa do álbum Foo Fighters" width="80" height="80"/>
+            </div>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
       </footer>
     </div>
   );
